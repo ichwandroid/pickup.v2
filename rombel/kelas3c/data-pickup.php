df74c7a2e9cbe9a33b6fe63ea485a2d0f832eeff
Ichwan <ichwan.laruku@gmail.com>
2022-05-10 12:27:17 +0700

    zxc

--- rombel/kelas3c/data-pickup.php
+++ rombel/kelas3c/data-pickup.php
@@ -66,7 +66,7 @@
         </tr>
       </thead>
       <tbody>
-        <?php $crud->dataviewguru("SELECT * FROM tbl_siswa JOIN tbl_scan ON tbl_scan.NIS = tbl_siswa.NIS WHERE KELAS='1 C - UMMU KULTSUM' ORDER BY TIMESCAN DESC");?>
+        <?php $crud->dataviewguru("SELECT * FROM tbl_siswa JOIN tbl_scan ON tbl_scan.NIS = tbl_siswa.NIS WHERE KELAS='3 C - HALIMAH AS SA\'DIYAH R.A' ORDER BY TIMESCAN DESC");?>
       </tbody>
     </table>
     
